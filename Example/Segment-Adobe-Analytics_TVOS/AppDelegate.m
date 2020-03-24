4533d1a14df5a4a50f93e192e47af89bc66a40bb
benhumphries <imachumphries@me.com>
2020-03-24 17:42:53 -0400

    Adds Segment Adobe Integration setup code to TVOS example project

--- Example/Segment-Adobe-Analytics_TVOS/AppDelegate.m
+++ Example/Segment-Adobe-Analytics_TVOS/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "SEGAdobeIntegrationFactory.h"
+#import <Analytics/SEGAnalytics.h>
 
 @interface AppDelegate ()
 
@@ -16,30 +18,20 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    SEGAnalyticsConfiguration *config = [SEGAnalyticsConfiguration configurationWithWriteKey:@"YOUR_WRITE_KEY_HERE"];
+    
+    [config use:[SEGAdobeIntegrationFactory instance]];
+    [SEGAnalytics setupWithConfiguration:config];
+    [[SEGAnalytics sharedAnalytics] track:@"Video Playback Started"];
+    [[SEGAnalytics sharedAnalytics] track:@"Video Content Started"
+                               properties: @{ @"full_episode": @true }
+                                  options: @{
+                                      @"integrations": @{}
+                                  }];
+    
+    [[SEGAnalytics sharedAnalytics] flush];
+    [SEGAnalytics debug:YES];
     return YES;
 }
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
 
 @end
